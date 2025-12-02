a55340da1cdb43a7654e88056b5dc90dcedb067d
MountainEnjoyer <chauve.zakary@gmail.com>
2025-12-02 22:43:22 +0100

    Changes

--- Git.sh
+++ Git.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#git reset --soft head~1 pour suprimer le commit le plus recent en gardant le travail fait
+#git reset --hard head~1 pareil mais en perdant le travail
+
 clear
 git add *
 clear
