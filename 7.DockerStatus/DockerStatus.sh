91ca9928d1b4b8b002e9abc29d90119bb6a880f5
MountainEnjoyer <chauve.zakary@gmail.com>
2025-12-03 01:10:59 +0100

    DockerStatus Script

---
+++ 7.DockerStatus/DockerStatus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "==== Status check docker service ===="
+status="$(systemctl status docker | awk 'NR==5 {print}' | cut -d ':' -f 2 | cut -d '(' -f 1)"
+echo $status
+if [ $status = "active" ];
+then 
+    echo "Service is running fine"
+else 
+    echo "service is not running"
+fi
