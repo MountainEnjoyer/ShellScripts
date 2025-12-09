9676d24360d657da31df230cf88ba758a1fb3092
MountainEnjoyer <chauve.zakary@gmail.com>
2025-12-09 23:15:07 +0100

    Curl script

--- 15.Curl/README.md
+++ 15.Curl/README.md
@@ -3,3 +3,5 @@
   - -s: silent request, it will not show progres bar
   - -w: it will display the information on terminal
   - "%{http_code}": to get the http code, for success it's 200
+
+
---
+++ 15.Curl/curl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+URL="https://zakary.xyz"
+echo "%{http_code}"
+response=$(curl -s -w "%{http_code}" $URL)
+http1_code=$(tail -n1 <<< "$response")
+content=$(sed '$ d' <<< "$response")
+echo "$http1_code"
+if [ $http1_code == 200 ];
+then
+  echo "request worked fine"
+else
+  echo "send slack message that request is denied"
+fi
+echo "$content"
